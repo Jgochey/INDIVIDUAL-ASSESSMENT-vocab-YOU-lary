b3a9c460b2abac052a20ab3af3cb46f97b5ee09c
Josh Gochey <josh@gochey.net>
2024-08-31 13:29:55 -0500

    Setup in preparation to deploy on netlify

--- components/loginButton.js
+++ components/loginButton.js
@@ -1,8 +1,11 @@
+/* eslint-disable import/order */
 import { signIn } from '../utils/auth';
+// eslint-disable-next-line no-unused-vars
+import firebase from 'firebase';
 
 // GOOGLE LOGIN BUTTON
 const loginButton = () => {
-  const domString = '<button id="google-auth" class="btn btn-danger">GOOGLE LOGIN</button>';
+  const domString = '<button id="google-auth" class="btn btn-danger">LOGIN</button>';
   document.querySelector('#login-form-container').innerHTML = domString;
   document.querySelector('#google-auth').addEventListener('click', signIn);
 };
