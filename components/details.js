1312ade2c3830327336fe1e84c579a8901fa5baa
Josh Gochey <josh@gochey.net>
2024-09-07 17:16:35 -0500

    Fixed the view vocab functionality and cards

--- components/details.js
+++ components/details.js
@@ -22,21 +22,39 @@ const viewVocab = (item) => {
   clearDom();
 
   const domString = `
+  <div class="card" style="width: 60rem;">
+<div class="card-body">
+  <h5 class="card-title">${item.title}</h5>
+  <h6 class="card-subtitle mb-2 text-muted">Language: ${item.language}</h6>
+   <h4 class="card-subtitle mb-2 text-muted">Created on: ${item.timeSubmitted}</h4>
+   <hr>
+     <p class="card-text bold">${item.definition}</p>
+  <i class="btn btn-success fas fa-eye" id="view-Vocab-btn--${item.firebaseKey}"></i>
+  <i class="fas fa-edit btn btn-info" id="update-Vocab--${item.firebaseKey}"></i>
+  <i id="delete-Vocab-btn--${item.firebaseKey}" class="btn btn-danger fas fa-trash-alt"></i>
+</div>
+</div>`;
+
+  renderToDOM('#cards', domString);
+};
+
+/* <div class="card" style="width: 50rem;">
+      <div class="card-body flex flex-wrap flex-row">
   <div class="mt-5 d-flex flex-wrap">
      <div class="mt-5">
         <i class="fas fa-edit btn btn-info" id="update-Vocab--${item.firebaseKey}"></i>
         <i id="delete-Vocab-btn--${item.firebaseKey}" class="btn btn-danger fas fa-trash-alt"></i>
    </div>
-   <div class="text-white ms-5 details">
+   <div class="text-black ms-5">
      <h5>${item.title}</h5>
      Language: <a>${item.language}</a>
      Created on: <a>${item.timeSubmitted}</a>
+        <hr>
      <p>${item.definition || ''}</p>
-     <hr>  
-      </div>
-    </div>`;
 
-  renderToDOM('#cards', domString);
-};
+      </div>
+    </div>
+       </div>
+    </div>` */
 
 export { viewVocab, getSingleVocab, getVocabDetails };
